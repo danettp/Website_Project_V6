c7b2ad09aae132d77da2bed7840afa1de2b85653
danettp <17204@my.sanctamaria.school.nz>
2023-07-19 13:59:30 +1200

    Updated
    Changed format of create_post and rendered

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     app = create_app()  # Create a Flask application instance using the create_app function
     app.run(debug=True) # Run the Flask application in debug mode
 
-# Title Field for blog posts.
+# Title Field for blog posts
--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ import secrets
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Post, User, Comment, Like
-from .forms import RegistrationForm, UpdateAccountForm
+from .forms import RegistrationForm, UpdateAccountForm, PostForm
 from . import db
 
 # Create a Blueprint for the views
@@ -31,20 +31,17 @@ def blog():
 @views.route("/create-post", methods=['GET', 'POST'])
 @login_required
 def create_post():
-    if request.method == "POST":
-        text = request.form.get('text')
-        title = request.form.get('title')
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        post = Post(title=title, text=text, author=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created!', category='success')
+        return redirect(url_for('views.blog'))
 
-        if not text:
-            flash('Post cannot be empty', category='error')
-        else:
-            post = Post(text=text, title=title, author=current_user.id)
-            db.session.add(post)
-            db.session.commit()
-            flash('Post created!', category='success')
-            return redirect(url_for('views.blog'))
-
-    return render_template('create_post.html', user=current_user)
+    return render_template('create_post.html', form=form, user=current_user)
 
 # Route for deleting a post
 @views.route("/delete-post/<id>")
